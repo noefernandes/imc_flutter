6c8ae77fc3aa02578bde9e59449086cd01949226
noefernandes <noe.fernandes.016@ufrn.edu.br>
2023-03-05 19:49:57 -0300

    final

---
+++ android/app/src/main/kotlin/com/example/imc_2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.imc_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ui/home.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+import 'result.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  TextEditingController pesoController = TextEditingController();
+  TextEditingController alturaController = TextEditingController();
+
+  GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+
+  void _resetCampos() {
+    pesoController = TextEditingController();
+    alturaController = TextEditingController();
+    _formKey.currentState!.reset();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Calculadora de IMC"),
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent,
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.refresh),
+            onPressed: _resetCampos,
+          )
+        ], //<Widget>[]
+      ), // app bar
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              const Icon(Icons.person, size: 120, color: Colors.blueAccent),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                    labelText: "Peso (kg)",
+                    labelStyle: TextStyle(color: Colors.blueAccent),
+                    focusedErrorBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(width: 2, color: Colors.redAccent)),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(width: 2, color: Colors.greenAccent))),
+                textAlign: TextAlign.center,
+                style:
+                    const TextStyle(color: Colors.blueAccent, fontSize: 25.0),
+                controller: pesoController,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Insira seu peso!";
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              const Padding(padding: EdgeInsets.fromLTRB(0, 10, 0, 10)),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                    labelText: "Altura (m)",
+                    labelStyle: TextStyle(color: Colors.blueAccent),
+                    focusedErrorBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(width: 2, color: Colors.redAccent)),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(width: 2, color: Colors.greenAccent))),
+                textAlign: TextAlign.center,
+                style:
+                    const TextStyle(color: Colors.blueAccent, fontSize: 25.0),
+                controller: alturaController,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Insira sua altura!";
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              Padding(
+                  padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+                  child: Container(
+                      height: 50.0,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            _calcular();
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                            //  primary: Colors.blue,
+                            textStyle: const TextStyle(
+                                fontSize: 30, fontWeight: FontWeight.bold)),
+                        child: const Text(
+                          "Calcular",
+                          style: TextStyle(color: Colors.white, fontSize: 20.0),
+                        ),
+                      ))),
+            ], //<widget>[]
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _calcular() {
+    String _texto = "";
+    String _imagem = "";
+
+    double peso = double.parse(pesoController.text);
+    double altura = double.parse(alturaController.text);
+
+    double imc = peso / (altura * altura);
+    //debugPrint("Peso ${peso} e altura ${altura}");
+    //debugPrint("$imc");
+    if (imc < 18.6) {
+      _texto = "Abaixo do peso (${imc.toStringAsPrecision(4)})";
+      _imagem = "imagens/thin.png";
+    } else if (imc >= 18.6 && imc < 24.9) {
+      _texto = "Peso ideal (${imc.toStringAsPrecision(4)})";
+      _imagem = "imagens/shape.png";
+    } else if (imc >= 24.9 && imc < 29.9) {
+      _texto = "Levemente acima do peso (${imc.toStringAsPrecision(4)})";
+      _imagem = "imagens/fat.png";
+    } else if (imc >= 29.9 && imc < 34.9) {
+      _texto = "Obesidade Grau I (${imc.toStringAsPrecision(4)})";
+      _imagem = "imagens/fat.png";
+    } else if (imc >= 34.9 && imc < 39.9) {
+      _texto = "Obesidade Grau II (${imc.toStringAsPrecision(4)})";
+      _imagem = "imagens/fat.png";
+    } else if (imc >= 40) {
+      _texto = "Obesidade Grau III (${imc.toStringAsPrecision(4)})";
+      _imagem = "imagens/fat.png";
+    }
+
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => Result(_imagem, _texto)));
+  }
+}
